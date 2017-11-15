077dd8e638a9e68a379594ded4c90c32707d6772
Iram Lee <iram.lee@gmail.com>
2017-11-15 15:38:30 -0600

    clean up Client class

--- MyBooks/mybooks/Client.java
+++ MyBooks/mybooks/Client.java
@@ -14,6 +14,68 @@ import java.util.*;
 public class Client {
 	
 	private Client() {}
+
+
+/**
+ * search() method
+ * ---------------
+ * Returns all books under the specified topic
+ * */
+	public void search(String topic, Store stub) {
+		try {
+			ArrayList<Book> searchResp = stub.search(topic);
+			if( searchResp != null) {
+				for(Book book : searchResp) {
+					System.out.println("Book title: " + book.bookTitle);
+					System.out.println("Item number: " + book.itemNumber);
+					System.out.println("---------------");
+				}
+			}else {
+				System.out.println("No results found under topic: " + topic);
+			}
+		}catch(Exception e) {
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+	
+	
+/**
+ * lookup() method
+ * ---------------
+ * Returns information about the requested item number
+ * */
+	public void lookup(int itemNum, Store stub) {
+		try {
+			Book lookupBook = stub.lookup(itemNum);
+			if(lookupBook != null) {
+				System.out.println("Qty in stock: " + lookupBook.stockQty);
+				System.out.println("Item cost: $" + lookupBook.bookCost);
+				System.out.println("Book topic: " + lookupBook.bookTopic);
+			}else {
+				System.out.println("Item number not found");
+			}
+		}catch(Exception e) {
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
+	
+	
+/**
+ * order() method
+ * --------------
+ * Return success / failed ordering result
+ * */
+	public void order(int itemNum, Store stub) {
+		try {
+			String orderMsg = stub.order(itemNum);
+			System.out.println(orderMsg);
+		}catch(Exception e) {
+			System.err.println("Client exception: " + e.toString());
+			e.printStackTrace();
+		}
+	}
 	
 	
 	/**
@@ -23,36 +85,14 @@ public class Client {
 		
 		//Setting the host to null indicates that the local host address should be used
 		String host = null;
+		Client clientApp = new Client();
 		try {
 			Registry registry = LocateRegistry.getRegistry(host);
 			Store stub = (Store) registry.lookup("Store");
 			
-			// *** Search *** //
-			ArrayList<Book> searchResp = stub.search("Graduate School");
-			if( searchResp != null) {
-				for(Book book : searchResp) {
-					System.out.println("Book title: " + book.bookTitle);
-					System.out.println("Item number: " + book.itemNumber);
-					System.out.println("---------------");
-				}
-			}else {
-				System.out.println("No results found under topic: ");
-			}
-			
-			// *** Lookup *** //
-			Book lookupBook = stub.lookup(2);
-			if(lookupBook != null) {
-				System.out.println("Qty in stock: " + lookupBook.stockQty);
-				System.out.println("Item cost: $" + lookupBook.bookCost);
-				System.out.println("Book topic: " + lookupBook.bookTopic);
-			}else {
-				System.out.println("Item number not found");
-			}
-			
-			// ** Order ** //
-			String orderMsg = stub.order(2);
-			System.out.println(orderMsg);
-			
+			clientApp.search("Graduate School", stub);
+			clientApp.lookup(2, stub);
+			clientApp.order(2, stub);
 		} catch(Exception e) {
 			System.err.println("Client exception: " + e.toString());
 			e.printStackTrace();
